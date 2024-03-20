d9ff129293c774244fccf973c594ed06b84718df
cholthi <cholthipaul@gmail.com>
2024-03-20 12:32:21 +0200

    added 0-async_generator.py file

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""
+Provides an async generator that sleeps 1s between yields
+"""
+import asyncio
+import random
+
+async def async_generator():
+    """loops 10 times and waits for 1s asycronously,
+    then yields a random value between 0 and 10
+    """
+    for i in range(10):
+        result = random.random() * 10
+        await asyncio.sleep(1)
+        yield result
